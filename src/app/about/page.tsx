3deb5da00c20335fe3887a23f2ce36e17b74c4cc
Ahmed Frag <a.frag166@gmail.com>
2026-06-16 14:45:34 +0300

    add: page metadata title & description

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,1 +1,8 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "About Us | Stride",
+    description: "Learn about our story and mission",
+};
+
 export { default } from "@/features/about";
