52ee899fe9b6f8a8511c1210aee96311fbee57c1
Ahmed Frag <a.frag166@gmail.com>
2026-06-22 16:32:03 +0300

    feat: add Wishlist module with metadata and error handling

---
+++ src/Modules/Wishlist/index.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Wishlist | Stride",
+    description:
+        "Your saved items. Keep track of products you love and buy them when you're ready.",
+};
+
+const Wishlist = () => {
+    return <div>Wishlist</div>;
+};
+
+export default Wishlist;
