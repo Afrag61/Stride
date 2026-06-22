5fc00c9e638307a3193b05b498f24cad9e65fe81
Ahmed Frag <a.frag166@gmail.com>
2026-06-22 15:27:22 +0300

    refactor: improve error handling in ProductDetails component by using ClientThrower for error display

--- src/Modules/ProductDetails/components/Details/index.tsx
+++ src/Modules/ProductDetails/components/Details/index.tsx
@@ -3,6 +3,7 @@ import { TProduct } from "@/types";
 import ProductDetails from "./ProductDetails";
 import { notFound } from "next/navigation";
 import { ErrorMessages } from "@/Modules/Error/enum";
+import ClientThrower from "@/Modules/Error/ClientThrower";
 
 interface Props {
     productId: string;
@@ -21,10 +22,7 @@ const Index: React.FC<Props> = async ({ productId }) => {
 
     if (error?.code === "PGRST116") notFound();
 
-    if (error)
-        throw new Error("Failed to Load Product", {
-            cause: "LOAD_PRODUCT_FAILED" satisfies keyof typeof ErrorMessages,
-        });
+    if (error) return <ClientThrower cause="LOAD_PRODUCT_FAILED" />;
 
     return <ProductDetails product={product} />;
 };
