484e64d5baf84c97610c81617fa90819da3ad132
Ahmed Frag <a.frag166@gmail.com>
2026-08-21 04:28:44 +0300

    feat: remove user authentication checks from FeaturedList, NewProductsList, Suggestion, and WishListProducts components

--- src/Modules/Home/components/Products/New/NewList.tsx
+++ src/Modules/Home/components/Products/New/NewList.tsx
@@ -2,10 +2,8 @@ import ProductItem from "@/components/UI/ProductItem";
 import ErrorMessage from "@/components/UI/ErrorMessage";
 import { createClient } from "@/lib/supabase/server";
 import { TProductList } from "@/types";
-import { getUser } from "@/Modules/Auth/lib/getUser";
 
 const NewProductsList = async () => {
-    const user = await getUser();
     const supabase = await createClient();
 
     const { data, error } = await supabase
@@ -21,11 +19,7 @@ const NewProductsList = async () => {
     return (
         <div className="mt-12 grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
             {products.map((product) => (
-                <ProductItem
-                    {...product}
-                    key={product.name}
-                    isAuthenticated={!!user}
-                />
+                <ProductItem {...product} key={product.name} />
             ))}
         </div>
     );
