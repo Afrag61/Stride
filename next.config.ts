345f71728f440ae32b4ae5d3b2c2669f8315f8c8
Ahmed Frag <a.frag166@gmail.com>
2026-08-27 17:12:36 +0300

    feat: add experimental optimizePackageImports configuration for lucid-react and react-icons

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,9 @@ const nextConfig: NextConfig = {
             },
         ],
     },
+    experimental: {
+        optimizePackageImports: ["lucid-react", "react-icons"],
+    },
 };
 
 export default nextConfig;
